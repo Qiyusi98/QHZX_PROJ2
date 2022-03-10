a999905358aee3face14c89ac601f061d935a86c
Qiyusi98 <635738678@qq.com>
2022-03-09 22:16:45 -0600

    Update sendfile.c
    sendfile version 1 without packet design and send id

--- sendfile.c
+++ sendfile.c
@@ -14,15 +14,14 @@
 #define MAX_FILE_SIZE 30000000
 #define MSB_MASK 0xFFFF0000
 #define LSB_MASK 0xFFFF
-
-unsigned int crc32b(char *message, long msg_len) {
-    int i, j;
+unsigned int crc32b(char *message, long message_len) {
+  int i, j;
     unsigned int byte, crc, mask;
 
     i = 0;
     crc = 0xFFFFFFFF;
     // while (message[i] != 0) {
-    while (i < msg_len) {
+    while (i < message_len) {
         if (message[i] == 0)
         {
             i = i + 1;
@@ -39,277 +38,175 @@ unsigned int crc32b(char *message, long msg_len) {
    return ~crc;
 }
 
-char csum(char *packet, int cnt) {
-    unsigned long pack_sum = 0;
-    while(cnt > 0) {
-        pack_sum += *packet++;
-        if (pack_sum & MSB_MASK) { /* standard checksum */
-            pack_sum = pack_sum & LSB_MASK;
-            pack_sum++; /* increment sum */
+int main(int argc, char **argv) {
+
+  /* check number of command line parameters */
+  if (argc != 5) {
+    printf(
+        "Invalid command line input, should have four parameters \
+            in the format of -r <recv host>:<recv port> -f <subdir>/<filename>.");
+    exit(1);
+  }
+
+  /* command line input */
+  char *receiver_info_input = argv[2];
+  char *file_info_input = argv[4];
+
+  char *host;
+  unsigned short port;
+  char *subdir;
+  char *filename;
+
+  /* sendfile socket */
+  int sockfd;
+  struct timeval tv;
+  tv.tv_sec = 2;
+  tv.tv_usec = 0;
+
+  /* variables for identifying the server */
+  struct sockaddr_in sin;
+  socklen_t addr_len = sizeof(struct sockaddr_in);
+  
+  /* packet info */
+  const int DATA_LEN = 25000;
+  const int FILE_LEN = 60;
+  const int PACKET_LEN = 2;
+  const int CRC_LEN = 4;
+  int total_size = DATA_LEN + FILE_LEN + PACKET_LEN + CRC_LEN;
+  total_size = 25078; // only for test
+
+  short RECEIVE_SIZE = 3;
+  char *send_buffer;
+  char *receive_buffer;
+  char send_id;
+  char receive_id;
+  unsigned int crc;
+  int send_count;
+  int count;
+  int receive_count;
+
+  /* handle file */
+  FILE *fptr;
+  char file_path[sizeof(file_info_input)];
+  char file_path_padding[FILE_LEN];
+  char *file_data;
+  short bytes_read;
+
+  /* split terminal input to get receiver and file information */
+  char *receiver_info = strtok(receiver_info_input, ":");
+  if (receiver_info == NULL) {
+    printf(
+        "Invalid command line input, should contain host and port\
+              and in the format of host:port.");
+    exit(1);
+  }
+  host = receiver_info;
+  receiver_info = strtok(NULL, ":");
+  if (receiver_info == NULL) {
+    printf(
+        "Invalid command line input, should contain host and port\
+              and in the format of host:port.");
+    exit(1);
+  }
+  port = atoi(receiver_info);
+
+  strcpy(file_path, file_info_input);
+  char *file_info = strtok(file_info_input, "/");
+  if (file_info == NULL) {
+    printf(
+        "Invalid command line input, should contain subdirectory and filename\
+              and in the format of subdir/filename.");
+    exit(1);
+  }
+  subdir = file_info;
+  file_info = strtok(NULL, "/");
+  if (file_info == NULL) {
+    printf(
+        "Invalid command line input, should contain subdirectory and filename\
+              and in the format of subdir/filename.");
+    exit(1);
+  }
+  filename = file_info;
+  printf("Input from terminal %s, %hu, %s, %s\n", host, port, subdir, filename);
+
+  /* create a socket */
+  if ((sockfd = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+    perror("opening UDP socket error");
+    abort();
+  }
+  if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv, sizeof tv) < 0) {
+    perror("setting UDP socket option error");
+    abort();
+  }
+
+  /* fill in the receiver's address */
+  memset(&sin, 0, sizeof(sin));
+  sin.sin_family = AF_INET;
+  sin.sin_addr.s_addr = inet_addr(host);
+  sin.sin_port = htons(port);
+
+  /* initialize variables */
+  send_id = (char)0;
+  strncpy(file_path_padding, file_path, FILE_LEN);
+  send_buffer = malloc(total_size);
+  receive_buffer = malloc(RECEIVE_SIZE);
+  
+
+  /* operate send file */
+  fptr = fopen(file_path, "r");
+  if (!fptr) {
+    printf("open file error");
+    abort();
+  }
+
+  file_data = (char *)malloc(total_size * sizeof(char));
+  while ((bytes_read = fread(file_data, 1, DATA_LEN, fptr)) > 0) {
+    memset(send_buffer, 0, total_size);
+
+    // TODO packet design may change
+    memset(send_buffer, 0, 1);
+    memset(send_buffer + 1, send_id, 1);
+    *(short *)(send_buffer + 2) = htons(bytes_read);
+    memcpy(send_buffer + 4, file_path_padding, FILE_LEN);
+    memcpy(send_buffer + 64, file_data, DATA_LEN);
+
+    memset(file_data, 0, DATA_LEN);
+
+    crc = crc32b(send_buffer, DATA_LEN + FILE_LEN + PACKET_LEN);
+    memcpy(send_buffer + (DATA_LEN + FILE_LEN + PACKET_LEN), &crc, CRC_LEN);
+
+    while (1) {
+      send_count = 0;
+      while (send_count < total_size) {
+        count = sendto(sockfd, (const char *)send_buffer, total_size, 0, (const struct sockaddr *)&sin, sizeof(sin));
+        if (count <= 0) {
+          perror("send socket error");
+          abort();
         }
-        cnt-=1;
-    }
-    return (pack_sum & LSB_MASK); /* final bit-wise and */
-}
+        send_count += count;
+      }
 
-time_t getCurrentTime(){
-        time_t currtime;
-        currtime = time(NULL);
-        return currtime;
-}
+      receive_count = recvfrom(sockfd, receive_buffer, RECEIVE_SIZE, MSG_WAITALL, (struct sockaddr *)&sin, &addr_len);
+      if (receive_count <= 0) {
+        perror("receive ack packet from sendfile error");
+      } else {
+        receive_id = receive_buffer[1];
+        if ((char)receive_id == (char)send_id) {
 
-time_t getTimeElapsed(time_t end_time, time_t start_time){
-        return (end_time - start_time);
-}
-
-time_t sum_time;
-
-int main(int argc, char *argv[])
-{    
-    /* 
-     * DGRAM Packet Structure:
-     * Packet Type: ACK or Data (1 Bit - 0:Data, 1: Ack)
-     * Identifier: Stop and Wait (1 Bit) 
-     * Packet Size: 2 Bytes short int
-     * Advertised Window: x
-     * Directory: 50 Bytes
-     * File name: 20 Bytes
-     * Data: 1000 Bytes
-     * CRC Error Code: 4 Bytes
-     */
-
-    // Handle Command Line Inputs
-    if (argc!=5)
-    {
-        printf("Incorrect number of arguments!\n");
-        exit(1);
-    }
-    
-    // char* rFlag = argv[1];
-    char* host_port = argv[2];
-    // char* fFlag = argv[3];
-    
-    char* dir_name = argv[4];
-    char fopen_file[sizeof(dir_name)];
-    strcpy(fopen_file, dir_name);
-
-    char* ptr1 = strtok(host_port, ":");
-    char* hostc = ptr1;
-    ptr1 = strtok(NULL, ":");
-    char* portc = ptr1;
-
-    char* ptr2 = strtok(dir_name, "/");
-    char* dir = ptr2;
-    ptr2 = strtok(NULL, "/");
-    char* fileName = ptr2;
-
-    unsigned short port = atoi(portc);
-    int sockfd;
-    short ack_size = 3;
-    char rcv_buffer[ack_size];
-
-    // unsigned int server_address;
-    struct sockaddr_in s_in;
-    
-    char *file_data;
-
-
-    int DATA_SIZE = 25000;
-    int HEADER_SIZE = 74;
-    int CRC_SIZE = 4;
-    long packet_size = DATA_SIZE + HEADER_SIZE + CRC_SIZE;
-
-    // Create a UDP Socket
-    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
-    {
-        perror("Socket creation failed");
-        exit(EXIT_FAILURE);
-    }
-    memset(&s_in, 0, sizeof(s_in));
-    s_in.sin_family = AF_INET;
-    s_in.sin_port = htons(port);
-    // s_in.sin_addr.s_addr = htons(inet_addr(hostc));
-    s_in.sin_addr.s_addr = inet_addr(hostc);
-
-    
-
-    // Read the FILE and breakdown to package into PACKETS.
-    FILE *fp;
-    fp = fopen(fopen_file, "rb");
-    if (!fp)
-    {
-        printf("Unable to open file.\n");
-        return 1;
-    }
-    printf("File open succeeded.\n");
-
-
-    file_data = (char *)malloc(packet_size * sizeof(char)); // 
-    short bytes_read;
-    int send_cnt;
-    int tmp_cnt;
-    socklen_t addr_len = sizeof(struct sockaddr_in);
-    size_t total_bytes = 0;
-    char *packet_msg;
-    packet_msg = malloc(packet_size);
-    char directory[50]; 
-    strncpy(directory, dir, 50);
-    char name[20];
-    strncpy(name, fileName, 20);
-
-    short sentID = (short)1;
-    int total_bytes_sent = 0;
-
-    double PRRT = 10;
-    double offset = 2;
-    struct timeval tv;
-    // tv.tv_sec = 3; // initial timeout is 1 seconds
-    // tv.tv_usec = 0;
-    // setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv, sizeof tv); // Set the timeout value
-    clock_t begin = clock();
-    printf("clock begin %f\n", (double)begin);
-    while ((bytes_read = fread(file_data, 1, DATA_SIZE, fp)) > 0)
-    {
-        memset(packet_msg, 0, packet_size);
-        total_bytes += bytes_read;
-      
-        // memset(packet_msg, 0, 1);
-        *(short*)packet_msg = htons(sentID);
-        // packet_msg[1] = (char)0;
-        
-        *(short*)(packet_msg+2) = htons(bytes_read);
-        memcpy(packet_msg+4, directory, 50); // Directory information
-        memcpy(packet_msg+54, name, 20); // File Name
-        memcpy(packet_msg+74, file_data, DATA_SIZE); // Actual Data
-
-        memset(file_data, 0, DATA_SIZE);
-
-        // Compute CRC
-        unsigned int crc = crc32b(packet_msg, DATA_SIZE + HEADER_SIZE);
-        memcpy(packet_msg + (DATA_SIZE+HEADER_SIZE), &crc, CRC_SIZE);
-        // printf("crc  %d\n", crc);
-
-        //double estimated_rtt = 1.00;
-        //double dev_rtt = 1.00;
-        //double timeout;
-        while (1){
-            // send packet
-            // recv ack
-            //if recvid == sendid
-            //      change sendID
-            //      break
-            // time_t start_time = getCurrentTime();
-            
-            //time_t end_time, pack_end_time;
-            // time_t end_time;
-            send_cnt = 0;
-            while (send_cnt < packet_size)
-            {
-                tmp_cnt = sendto(sockfd, (const char *)packet_msg, packet_size, 0, (const struct sockaddr *) &s_in, sizeof(s_in));
-                if(tmp_cnt <= 0){
-                    printf("Error sending!\n");
-                    return 1;
-                }
-                send_cnt += tmp_cnt;
-            }
-            //pack_end_time = getCurrentTime();
-            //double send_time = (double)getTimeElapsed(pack_end_time,start_time);
-            total_bytes_sent += send_cnt;
-            printf("[send data] %ld %d\n", total_bytes-bytes_read, bytes_read);
-
-            tv.tv_sec = 2;//2 * (int)PRRT; // initial timeout is 1 seconds
-            // tv.tv_usec = (PRRT - (int)PRRT) * 1000000;
-            setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv, sizeof tv); // Set the timeout value
-
-
-            // printf("clock begin %f\n", (double)begin);
-
-            // printf("clock  %f\n", (double)clock());
-            int bytes_rcvd = recvfrom(sockfd, rcv_buffer, ack_size, MSG_WAITALL, (struct sockaddr *)&s_in, &addr_len);
-            // printf("clock  %f\n", (double)clock());
-
-            if (bytes_rcvd > 0)
-            {
-                // end_time = getCurrentTime();
-               
-                
-                //estimated_rtt  = 0.875*(double)estimated_rtt + 0.125*(double)getTimeElapsed(end_time,start_time); //estimated_rtt  for smoothing
-                //dev_rtt  = 0.75*dev_rtt + 0.25*(abs(estimated_rtt - (double)getTimeElapsed(end_time,start_time))); //calculate deviations
-                //fprintf(stderr, "Estimated rtt: %f\n", estimated_rtt);
-                //fprintf(stderr, "Deviation rtt: %f\n", dev_rtt);
-                //timeout = estimated_rtt + 4*dev_rtt; // set timeout
-                //timeout = timeout-send_time; // adjust for recv
-                // fprintf(stderr,"Time out:%f ",timeout);
-                
-                //double sec, msec;
-                //msec = modf(timeout,&sec);
-                //msec = msec*1000000;//convert sec to usec to feed into tv_usec
-                //struct timeval ntv;
-                //ntv.tv_sec = sec;
-                //ntv.tv_usec = msec;
-                //setsockopt(sockfd,SOL_SOCKET,SO_RCVTIMEO, (const char*)&ntv, sizeof ntv );//recv timeout
-
-                // char csum_ack = csum(rcv_buffer, 2);
-                // if (csum_ack != rcv_buffer[2])
-                // {
-                //     printf("[recv corrupt packet]\n");
-                //     printf("Resending packet since ACK is corrupted\n");
-                //     continue; // If error detected, discard the packet
-                // }
-
-                // printf("Ack received!\n");
-                short rcvID;
-                // rcvID = *(rcv_buffer+1);
-                rcvID = (short)ntohs(*(short *)(rcv_buffer));
-                printf("receive ACK: rcvID=%d\n", (int)rcvID);
-                if ((short)rcvID == (short)sentID)
-                {
-                    //struct timeval ntv;
-                    //ntv.tv_sec = cumulative_timeout;
-                    //ntv.tv_usec = 0;
-                    //fprintf(stderr,"The new timeout is:%ld\n",cumulative_timeout);
-                    //set new timeout value
-                    //setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (const char*)&ntv, sizeof ntv); // Set the timeout value
-
-                    clock_t end = clock();
-                    printf("clock end %f\n", (double)end);
-                    double time_taken = ((double)(end-begin) / CLOCKS_PER_SEC) * 1000 + offset; // Add the accumulated TO value
-                    begin = clock();
-                    // double time_taken = ((double)t)/CLOCKS_PER_SEC;
-                    printf("Time Taken %f\n", time_taken);
-                    PRRT = 0.5 * PRRT + 0.5 * time_taken;
-                    fprintf(stderr,"This is the adaptive timeout: %f\n",PRRT);
-                    printf("clock begin %f\n", (double)begin);
-
-                    offset = 2; // If successfully receive ACK, reset the offset
-                    // if (sentID == 1)
-                    // {
-                    //     sentID = (char)0;
-                    // }
-                    // else if (sentID == 0)
-                    // {
-                    //     sentID = (char)1;
-                    // } 
-                    sentID++;
-                    break;
-                }
-                else
-                {
-                    printf("Resending packet since ACK ID different from Sent ID\n");
-                }
-            }
-            else
-            {
-                offset = offset + PRRT; // if timeout, TO value should be added to estimate the RTT
-                perror("Error Receiving Ack");
-                printf("Resending packet\n");
-            }
-        
+          // TODO change to sequence number
+          if (send_id == 1) {
+            send_id = (char)0;
+          } else if (send_id == 0) {
+            send_id = (char)1;
+          }
+          break;
         }
-
+      }
     }
-    printf("[completed]\n");
-    // Close socket descriptor and exit.
-    close(sockfd);
-    return 0;
+  }
+
+  printf("[completed]\n");
+  close(sockfd);
+  return 0;
 }
+
